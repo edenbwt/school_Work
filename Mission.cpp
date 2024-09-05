262400ff8602032ab2f01d91f35e903c3c209d88
EdenBwt <carl.guihot.pr@gmail.com>
2024-09-05 10:54:12 +0200

    cree la base de la class Mission

---
+++ Mission.cpp
@@ -0,0 +1,4 @@
+#include "Mission.h"
+
+using namespace std;
+
--- Mission.h
+++ Mission.h
@@ -1,1 +1,26 @@
-#pragma once
+#ifndef MISSION_H
+#define MISSION_H
+#include <iostream>
+#include <string>
+
+
+
+class Mission {
+
+public:
+
+
+
+private:
+
+
+
+protected:
+
+
+
+
+
+
+};
+#endif
